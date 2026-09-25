a54b2b2b9468a43f5f586874e15064aa869ef60c
Dhenu <dhenu@example.com>
2026-09-25 15:46:44 +0530

    Library build success

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,32 @@ import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import { playwright } from '@vitest/browser-playwright';
+import { resolve } from 'path';
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
+  
+  // Library Build Configuration for NPM Publishing
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'), // Tamara saare components export karva mate src/index.ts file banaavo
+      name: 'DhenuUIComponents',
+      fileName: (format) => `index.${format === 'es' ? 'js' : 'umd.cjs'}`
+    },
+    rollupOptions: {
+      // Externalize react so it won't be bundled inside library
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  },
+
+  // Vitest and Storybook Testing Configuration
   test: {
     projects: [{
       extends: true,
